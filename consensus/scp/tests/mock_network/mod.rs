f31d65fe2726553eb31f378b49801e9b8f2a8456
Robb Walters <rjwalters@gmail.com>
2020-06-16 19:43:07 -0700

    Revert "why lock?"
    This reverts commit 8637003572e630cbd6f201d39e3ab797e862e2c9.

--- consensus/scp/tests/mock_network/mod.rs
+++ consensus/scp/tests/mock_network/mod.rs
@@ -387,6 +387,8 @@ impl SimulatedNode {
 
                                 let outgoing_msg: Option<Msg<String>> = {
                                     thread_local_node
+                                        .lock()
+                                        .expect("thread_local_node lock failed when nominating value")
                                         .nominate(
                                             current_slot as SlotIndex,
                                             BTreeSet::from_iter(values_to_nominate)
@@ -405,6 +407,8 @@ impl SimulatedNode {
                         for msg in incoming_msgs.iter() {
                             let outgoing_msg: Option<Msg<String>> = {
                                 thread_local_node
+                                    .lock()
+                                    .expect("thread_local_node lock failed when handling msg")
                                     .handle(msg)
                                     .expect("node.handle_msg() failed")
                             };
@@ -418,6 +422,8 @@ impl SimulatedNode {
                         // Process timeouts (for all slots)
                         let timeout_msgs: Vec<Msg<String>> = {
                             thread_local_node
+                                .lock()
+                                .expect("thread_local_node lock failed when processing timeouts")
                                 .process_timeouts()
                                 .into_iter()
                                 .collect()
@@ -430,6 +436,8 @@ impl SimulatedNode {
                         // Check if the current slot is done
                         let new_block:Vec<String> = {
                             thread_local_node
+                              .lock()
+                              .expect("thread_local_node lock failed when collecting externalized values")
                               .get_externalized_values(current_slot as SlotIndex)
                         };
 
