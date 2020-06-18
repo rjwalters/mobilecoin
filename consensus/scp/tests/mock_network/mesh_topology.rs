1097060ed49334c1b0d1fb2aaf31c592cf8102bd
Robb Walters <rjwalters@gmail.com>
2020-06-18 12:33:29 -0700

    change names

--- consensus/scp/tests/mock_network/mesh_topology.rs
+++ consensus/scp/tests/mock_network/mesh_topology.rs
@@ -26,7 +26,7 @@ pub fn dense_mesh(num_nodes: usize, k: usize) -> mock_network::Network {
             .collect::<Vec<NodeID>>();
 
         nodes.push(mock_network::NodeOptions::new(
-            format!("{}", node_index),
+            format!("m{}", node_index),
             test_utils::test_node_id(node_index as u32),
             peers_vector.iter().cloned().collect::<HashSet<NodeID>>(),
             QuorumSet::new_with_node_ids(k as u32, peers_vector),
