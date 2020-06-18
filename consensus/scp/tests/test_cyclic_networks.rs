0eeb43b5673bc72127ea4d136b8075b3ea8bee3a
Robb Walters <rjwalters@gmail.com>
2020-06-18 10:49:42 -0700

    focus on externalized duplicates

--- consensus/scp/tests/test_cyclic_networks.rs
+++ consensus/scp/tests/test_cyclic_networks.rs
@@ -20,36 +20,8 @@ fn cyclic_test_helper(num_nodes: usize, logger: Logger) {
 
 #[test_with_logger]
 #[serial]
-fn cyclic_1(logger: Logger) {
-    cyclic_test_helper(1, logger);
-}
-
-#[test_with_logger]
-#[serial]
 fn cyclic_2(logger: Logger) {
-    cyclic_test_helper(2, logger);
-}
-
-#[test_with_logger]
-#[serial]
-fn cyclic_3(logger: Logger) {
-    cyclic_test_helper(3, logger);
-}
-
-#[test_with_logger]
-#[serial]
-fn cyclic_4(logger: Logger) {
-    cyclic_test_helper(4, logger);
-}
-
-#[test_with_logger]
-#[serial]
-fn cyclic_5(logger: Logger) {
-    cyclic_test_helper(5, logger);
-}
-
-#[test_with_logger]
-#[serial]
-fn cyclic_6(logger: Logger) {
-    cyclic_test_helper(6, logger);
+    for _i in 0..100 {
+        cyclic_test_helper(2, logger);
+    }
 }
